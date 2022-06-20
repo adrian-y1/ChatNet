9a2854cb9154eef0ce1405827b9830dd1c3b5a5c
adrian-y1 <adrian.yonan1@gmail.com>
2022-06-20 16:36:36 +1000

    Added Procfile

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # To store files in the file system instead of the database
 # Using os.path.join makes sure the path to the directly is created no matter the OS the current user is using

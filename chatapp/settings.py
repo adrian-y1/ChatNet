f80c61867b78cd63f844cc5029afacfc8f95b8fb
adrian-y1 <adrian.yonan1@gmail.com>
2022-06-20 17:33:53 +1000

    Added django-heroku

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from pickle import FALSE
+import django_heroku 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -161,3 +162,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())

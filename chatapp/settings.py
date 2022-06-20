1c586d751244e8914717c2229afe48da8f7a398c
adrian-y1 <adrian.yonan1@gmail.com>
2022-06-20 21:27:34 +1000

    Removed unsigned in user img

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://chatnet50.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['https://chatnet50.herokuapp.com/','localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://chatnet50.herokuapp.com/*',]
 

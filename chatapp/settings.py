28a9d26f6e47eca054effd072356fa278270849b
adrian-y1 <adrian.yonan1@gmail.com>
2023-04-15 17:32:07 +1000

    Updated settings.py's ALLOWED_HOST URL

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://chatnet.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['chatnet.up.railway.app', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://chatnet.up.railway.app/*',]
 
